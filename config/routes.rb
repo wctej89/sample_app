03e0f381cd751a1483fc345cc745f212735297b4
wctej89 <wctej89@gmail.com>
2012-09-26 01:08:14 -0700

    finish layouts2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  math '/signup', to: 'users#new'
+  match '/signup', to: 'users#new'
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
